5495c498774880aa3a26314da94ca2f33938a1a1
BootEmilio <emiorea8@gmail.com>
2024-10-14 08:36:07 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes'); // Rutas de autenticación
+const gymRoutes = require('./routes/gymRoutes');
+const trainerRoutes = require('./routes/trainerRoutes');
+const clientRoutes = require('./routes/clientRoutes');
+const authMiddleware = require('./middlewares/authMiddleware');
+const loggerMiddleware = require('./middlewares/loggerMiddleware');
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(loggerMiddleware);
+
+app.use('/api', authRoutes);
+
+app.use(authMiddleware);
+
+// Rutas protegidas por autenticación
+app.use('/api', gymRoutes);
+app.use('/api', trainerRoutes);
+app.use('/api', clientRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
