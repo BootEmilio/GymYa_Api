9e7769b1f55857a57e93c71e79735ba3a00531e4
BootEmilio <emiorea8@gmail.com>
2024-10-14 17:07:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/api', gymRoutes);
 app.use('/api', trainerRoutes);
 app.use('/api', clientRoutes);
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT|| 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
