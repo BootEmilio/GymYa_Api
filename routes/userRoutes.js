629f1bbbd444f7e12423ae9e63ab344251547e38
BootEmilio <emiorea8@gmail.com>
2024-11-04 00:14:22 -0600

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const loggerMiddleware = require('../middlewares/loggerMiddleware');
 
-router.post('/usuarios', loggerMiddleware, userController.crearUsuario);
+router.get('/usuarios', loggerMiddleware, userController.crearUsuario);
 router.get('/usuarios/:id', loggerMiddleware, userController.obtenerUsuario);
+router.post('/usuarios', loggerMiddleware, userController.crearUsuario);
 router.patch('/usuarios/:id', loggerMiddleware, userController.actualizarUsuario);
 router.delete('/usuarios/:id', loggerMiddleware, userController.eliminarUsuario);
 
