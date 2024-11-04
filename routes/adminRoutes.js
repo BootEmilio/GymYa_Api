5e108df89308b4d1bc32ef121e28accbf6746266
BootEmilio <emiorea8@gmail.com>
2024-11-03 21:57:41 -0600

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 const adminController = require('../controllers/adminController');
 const router = express.Router();
 
