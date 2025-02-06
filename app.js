e32e92745b521fd68c41730e037be72386c44aac
BootEmilio <emiorea8@gmail.com>
2025-02-06 10:35:15 -0600

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const loggerMiddleware = require('./middlewares/loggerMiddleware');
 const app = express();
 connectDB();
 
-app.use(cors({ origin: 'https://bootemilio.github.io' }));
-app.use(express.json());
+app.use(cors({
+  origin: ['https://gymya-web.vercel.app/', 'https://gymya-web-bootemilios-projects.vercel.app/'], // Múltiples orígenes
+}));
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
