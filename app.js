b77f590798cf9c914a4cce5f0234a26561441f09
BootEmilio <emiorea8@gmail.com>
2025-02-06 10:32:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ const loggerMiddleware = require('./middlewares/loggerMiddleware');
 const app = express();
 connectDB();
 
-app.use(cors({
-  origin: ['https://bootemilio.github.io', 'http://localhost:5173'], // Múltiples orígenes
-}));
+app.use(cors({ origin: 'https://bootemilio.github.io' }));
+app.use(express.json());
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
