0f9974d68bf33063fdf7e0048deea94924012248
levos04 <leviolevos@gmail.com>
2025-02-21 17:29:44 -0600

    Modelo de pagos

--- models/pagos.js
+++ models/pagos.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const PagoSchema = new mongoose.Schema({
-    usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+    membresia_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Mmembresia', required: true },
     gym_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Gimnasio', required: true },
     fecha_hora: { type: Date, default: Date.now},
     concepto: { type: String, required: true },
