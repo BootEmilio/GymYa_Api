97267a545b40ed1ce8d196c681ce92950bc49077
levos04 <leviolevos@gmail.com>
2025-03-04 12:00:17 -0600

    asistencias #2

--- services/asistenciasService.js
+++ services/asistenciasService.js
@@ -63,21 +63,6 @@ const registrarAsistencia = async (gymId, membresiaId, fecha_hora) => {
 //Servicio para que el administrador vea las asistencias (paginadas y poder cambiar los días a ver)
 const verAsistencias = async (gym_id, fecha, search = '', page = 1, limit = 10) => {
     try {
-        // Crear condición de búsqueda
-        let searchCondition = {};
-        if (search) {
-            const searchConditions = [
-                { 'usuario.nombre_completo': { $regex: search, $options: 'i' } },
-                { 'usuario.username': { $regex: search, $options: 'i' } }
-            ];
-
-            if (mongoose.Types.ObjectId.isValid(search)) {
-                searchConditions.push({ '_id': new mongoose.Types.ObjectId(search) });
-            }
-
-            searchCondition = { $or: searchConditions };
-        }
-
         // Obtener el rango de fechas (inicio y fin del día actual)
         let fechaInicio, fechaFin;
 
@@ -91,104 +76,25 @@ const verAsistencias = async (gym_id, fecha, search = '', page = 1, limit = 10) 
             fechaFin = new Date(fechaConsulta.setHours(23, 59, 59, 999)); // Fin del día consultado
         }
 
-        // Probar la consulta sin el lookup y el unwind para depurar
-        const asistenciasSinLookup = await Asistencia.find({
+        // Probar sin lookup ni unwind
+        const asistencias = await Asistencia.find({
+            gym_id: new mongoose.Types.ObjectId(gym_id),
+            fecha_hora: { $gte: fechaInicio, $lte: fechaFin }
+        })
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit));
+
+        const total = await Asistencia.countDocuments({
             gym_id: new mongoose.Types.ObjectId(gym_id),
             fecha_hora: { $gte: fechaInicio, $lte: fechaFin }
         });
 
-        console.log('Asistencias encontradas sin lookup:', asistenciasSinLookup);
-
-        const asistencias = await Asistencia.aggregate([
-            {
-                $match: {
-                    gym_id: new mongoose.Types.ObjectId(gym_id),
-                    fecha_hora: { $gte: fechaInicio, $lte: fechaFin } // Filtrar por el rango de fechas
-                }
-            },
-            {
-                $lookup: {
-                    from: 'usuarios', // Colección de usuarios
-                    localField: 'usuario_id', // Campo en asistencias
-                    foreignField: '_id', // Campo en colección de usuarios
-                    as: 'usuario'
-                }
-            },
-            { $unwind: '$usuario' }, // Descomprimir el array de usuarios
-            {
-                $match: searchCondition // Aplicar la condición de búsqueda
-            },
-            {
-                $group: {
-                    _id: {
-                        usuario_id: '$usuario_id',
-                        nombre_completo: '$usuario.nombre_completo'
-                    },
-                    asistencias: {
-                        $push: {
-                            asistencia_id: '$_id',
-                            fecha_hora: '$fecha_hora',
-                            tipo_acceso: '$tipo_acceso'
-                        }
-                    }
-                }
-            },
-            {
-                $sort: {
-                    'asistencias.fecha_hora': -1 // Ordenar cronológicamente las asistencias
-                }
-            },
-            {
-                $facet: {
-                    metadata: [{ $count: "total" }],
-                    data: [
-                        { $skip: (page - 1) * limit },
-                        { $limit: parseInt(limit) }
-                    ] // Paginación
-                }
-            }
-        ]);
-
-        const resultado = asistencias[0].data;
-        const total = asistencias[0].metadata.length > 0 ? asistencias[0].metadata[0].total : 0;
-
-        // Procesar el resultado para emparejar entradas con salidas
-        const asistenciasEmparejadas = resultado.map(usuario => {
-            const { _id, asistencias } = usuario;
-            const emparejadas = [];
-
-            let entradaActual = null;
-
-            asistencias.forEach(asistencia => {
-                if (asistencia.tipo_acceso === 'Entrada') {
-                    if (entradaActual) {
-                        emparejadas.push({ entrada: entradaActual, salida: null });
-                    }
-                    entradaActual = asistencia;
-                } else if (asistencia.tipo_acceso === 'Salida' && entradaActual) {
-                    emparejadas.push({ entrada: entradaActual, salida: asistencia });
-                    entradaActual = null;
-                }
-            });
-
-            if (entradaActual) {
-                emparejadas.push({ entrada: entradaActual, salida: null });
-            }
-
-            return {
-                usuario_id: _id.usuario_id,
-                nombre_completo: _id.nombre_completo,
-                asistencias: emparejadas
-            };
-        });
-
-        return { asistencias: asistenciasEmparejadas, total };
+        return { asistencias, total };
     } catch (error) {
         console.error(`Error al mostrar las asistencias del día ${fecha} para gymId: ${gym_id}:`, error);
         throw new Error(`Error al mostrar las asistencias del día ${fecha}`);
     }
 };
-
 
 //Servicio para que el usuario vea su última asistencia
 const verAsistencia = async (membresiaId) => {
